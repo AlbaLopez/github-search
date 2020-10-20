87c010f4b938b362b3d4e8f4ac3067cabb928d88
Alba <alba@keeeb.com>
2020-10-20 18:24:25 +0200

    fix: types for response and user

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -3,7 +3,32 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 
+class UserResponse {
+  incomplete_results: boolean;
+  items: User[];
+  total_count: number;
+}
+
 class User {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string;
+  html_url: string;
+  id: number;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  score: 1
+  site_admin: false
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  url: string;
 }
 
 @Injectable({
@@ -11,9 +36,11 @@ class User {
 })
 export class UsersService {
 
+  public users: User[];
+  
   constructor(private http: HttpClient) { }
 
-  public searchUsers(query): Observable<User> {
-    return this.http.get(`${environment.searchApi}users?q=${query}`)
+  public searchUsers(query): Observable<UserResponse> {
+    return this.http.get<UserResponse>(`${environment.searchApi}users?q=${query}`)
   }
 }
